6df9810f40c20e5e61126abb8a310169c4062179
vimlesh164 <vimlesh164@gmail.com>
2026-07-16 18:25:59 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   root: '.',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: './index.html'
-    }
+  server: {
+    port: 5173,
+    open: true
   }
-})
+});
